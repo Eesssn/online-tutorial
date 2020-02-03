42f902b47eb213ca2b7a717effe7e70c115ba39c
hossein <hossein@localhost.localdomain>
2020-02-03 12:09:20 +0330

    Disable Screen Capture/Screen Shot

--- android/app/src/main/java/com/app/MainActivity.java
+++ android/app/src/main/java/com/app/MainActivity.java
@@ -1,7 +1,8 @@
 package com.app;
 
 import com.facebook.react.ReactActivity;
-
+import android.os.Bundle;
+import android.view.WindowManager;
 public class MainActivity extends ReactActivity {
 
   /**
@@ -12,4 +13,9 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "app";
   }
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+    }
 }
